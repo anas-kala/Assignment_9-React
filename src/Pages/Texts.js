4c14657cc8b4d2325261b75c5514b8871cb3a01b
Anas Al Kala <anas.kala@hotmail.com>
2021-05-09 16:11:47 +0200

    fixing text

--- src/Pages/Texts.js
+++ src/Pages/Texts.js
@@ -13,39 +13,56 @@ const Div = styled.div`
 `;
 
 const Texts = (props) => {
-  const [value, setValue] = useState("male");
-  const [number, setNumber]=useState(1);
+  const [value, setValue] = useState("female");
+  const [number, setNumber] = useState(0);
 
-  function handleChange(event) {
-    setValue(event.target.value);
-  }
-  console.log(value);
   const { t, i18n } = useTranslation();
- 
 
   useEffect(() => {
     if (props.lang === "French") {
       i18n.changeLanguage("fr-CA");
     } else if (props.lang === "English") {
       i18n.changeLanguage("en-US");
-    }
-    else if (props.lang === "German") {
+    } else if (props.lang === "German") {
       i18n.changeLanguage("de-DE");
     }
   });
 
+  function getString(str) {
+    if (number === 0 || number === "") {
+      if (str === "apple") return <p>{t("noApple")}</p>;
+      if (value === "male" && str === "person")
+        return <p>{t("noPersonHis")}</p>;
+      if (value === "female" && str === "person")
+        return <p>{t("noPersonHer")}</p>;
+    } else if (number > 0) {
+      if (str === "apple") return <p>{t("apple")}</p>;
+      if (value === "male" && str === "person") return <p>{t("personHis")}</p>;
+      if (value === "female" && str === "person")
+        return <p>{t("personHer")}</p>;
+    }
+  }
+
+  console.log(value);
   return (
     <div>
       <Div>
-        <Input type="numbers" placeholder="Enter persons number" onChange={event => setNumber(event.target.value)}  />
-        <select value={value} onChange={handleChange}>
+        <Input
+          type="numbers"
+          placeholder="Enter persons number"
+          onChange={(event) => setNumber(event.target.value)}
+        />
+        <select
+          onChange={(event) => setValue(event.target.value)}
+          value={value}
+        >
           <option value="female">Female</option>
           <option value="male">Male</option>
         </select>
       </Div>
-      <h1>{t("Welcome to React")}</h1>
-      <h1>{t("a")}</h1>
-      <h1>{t("b")}</h1>
+      <h1>{t("welcome")}</h1>
+      <h1>{getString("apple")}</h1>
+      <h1>{getString("person")}</h1>
     </div>
   );
 };
