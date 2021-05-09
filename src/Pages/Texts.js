ab2b73d4d15d8106ce0e462664c5e08246caca70
Anas Al Kala <anas.kala@hotmail.com>
2021-05-09 14:09:00 +0200

    adding languages

--- src/Pages/Texts.js
+++ src/Pages/Texts.js
@@ -14,32 +14,38 @@ const Div = styled.div`
 
 const Texts = (props) => {
   const [value, setValue] = useState("male");
+  const [number, setNumber]=useState(1);
 
   function handleChange(event) {
     setValue(event.target.value);
   }
   console.log(value);
   const { t, i18n } = useTranslation();
-  // i18n.changeLanguage('fr-CA');
+ 
+
   useEffect(() => {
     if (props.lang === "French") {
       i18n.changeLanguage("fr-CA");
     } else if (props.lang === "English") {
       i18n.changeLanguage("en-US");
     }
+    else if (props.lang === "German") {
+      i18n.changeLanguage("de-DE");
+    }
   });
+
   return (
     <div>
       <Div>
-        <Input type="numbers" placeholder="Enter persons number" />
+        <Input type="numbers" placeholder="Enter persons number" onChange={event => setNumber(event.target.value)}  />
         <select value={value} onChange={handleChange}>
           <option value="female">Female</option>
           <option value="male">Male</option>
         </select>
       </Div>
       <h1>{t("Welcome to React")}</h1>
-      <h1>goodbye</h1>
-      <h1>On other Label</h1>
+      <h1>{t("a")}</h1>
+      <h1>{t("b")}</h1>
     </div>
   );
 };
--- src/i18n.js
+++ src/i18n.js
@@ -6,13 +6,42 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   en: {
     translation: {
-      "Welcome to React": "Welcome to React and react-i18next"
-    }
+      "Welcome to React": "Welcome to React and react-i18next",
+      "there is": "there is",
+      "there are": "there are",
+      "apple":"apple",
+      "apples":"apples",
+      "person": "person in his party",
+      "persons": "persons in his party",
+      "a":"there is one person in the party",
+      "b":"there is one apple",
+    },
   },
   fr: {
     translation: {
-      "Welcome to React": "Bienvenue à React et react-i18next"
-    }
+      "Welcome to React": "Bienvenue à React et react-i18next",
+      "there is": "il y a ",
+      "there are": "il y a ",
+      "apple":"pomme",
+      "apples":"pommes",
+      "person": "personne dans son parti",
+      "persons": "personnes de son parti",
+      "a":"il y a une personne dans le groupe",
+      "b":"il y a une pomme",
+    },
+  },
+  de: {
+    translation: {
+      "Welcome to React": "Wellkommen beiReact un React-i18next",
+      "there is": "es gibt",
+      "there are": "es sind",
+      "apple":"Apfel",
+      "apples":"Äpfel",
+      "person": "Person in der Party",
+      "persons": "Personen in der Party",
+      "a":"es gibt eine Person in der Party",
+      "b":"es gibt ein Apfel",
+    },
   }
 };
 
@@ -25,8 +54,8 @@ i18n
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
-  export default i18n;
+export default i18n;
